477837167a0380dc37361927c6b8d55efba441e1
Pavan Katepalli <pavankatepalli@Pavans-MBP-2.home>
2016-10-25 15:08:07 -0400

    put in conditionals

--- app/models/audit.rb
+++ app/models/audit.rb
@@ -1,4 +1,52 @@
 class Audit < ActiveRecord::Base
   belongs_to :instructor
   belongs_to :lesson_plan
+  # @@ is a class level method where you can access variables throughout the class
+
+  @@atLeastCheck = {
+    "vocal_enc_num" => 5,
+    "discuss_num" => 4,
+    "mov_aw_noi_num" => 4,
+    "mean_quest_num" => 15,
+    "directed_quest_num" => 10,
+    "anti_jargon_num" => 7,
+    "engage_num" => 7,
+    "overall_num" => 7,
+    "audio_qual_num" => 3
+  }
+
+  @@exact = {
+    "big_flag" => false,
+    "objs" => true,
+    "action_flag" => false,
+    "tangent_num" => 0,
+    "fifteen_noeng" => false,
+    "nervous" => false,
+    "percent_hp_lp" => "100%",
+    "code_is_no_big" => true
+  }
+
+  def exact_check(key, val)
+    if @@exact[key] == val
+      return true
+    else
+      return false
+    end
+  end
+
+  def at_least_check(key, val)
+    if (@@atLeastCheck[key] <= val)
+      return true
+    else
+      return false
+    end
+  end
+
+  def for_percent_lp p
+    if (p == "80% to 100%") || (p == "100%")
+      true
+    else
+      false
+    end
+  end
 end
