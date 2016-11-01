652111ebc664a606b299d91cfaea4c1a2b7caeda
Pavan Katepalli <pavankatepalli@Pavans-MBP-2.home>
2016-11-01 18:21:35 -0400

    admin user roles

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,31 @@
+class Users::SessionsController < Devise::SessionsController
+# before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  def create
+    binding.pry
+
+    unless audit_params.code == 'trilogy_rocks_man473'
+      redirect_to root_path
+    end
+
+    super
+  end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  resources :users, :except => [:new, :create]
 
   resources :student_success_managers
 
