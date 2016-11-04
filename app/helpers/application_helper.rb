e02aba22a55686f2f2e8d8de378ca25d4404761b
Pavan Katepalli <pavankatepalli@Pavans-MBP-2.home>
2016-11-04 12:16:47 -0400

    first name only

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def first_name s
+    s.split(" ").first
+  end
+
   # not sure why we need the else clause here - but prod breaks if it's not
   def awesome_choice a
     choice = a.give_comp
