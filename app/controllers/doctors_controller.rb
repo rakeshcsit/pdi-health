797913b5cc3714822ef543d0d42855576a986d95
Pavan Katepalli <pavankatepalli@Pavans-MBP-2.home>
2017-07-20 02:52:04 -0700

    ar call

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -37,6 +37,21 @@ class DoctorsController < ApplicationController
   end
 
   private
+    def set_patients
+      # get all the patients that don't have doctors assigned to them
+      # so find all patients that are not in the doctors_patients table and find all the patients in the doctors_patients table that are set to false
+      @patients = Patient
+      .joins('LEFT JOIN doctors_patients dp ON dp.patient_id = patients.id')
+      .where(dp.active != true)
+
+      # Topic.where( 'forum_id not in (?)', (@forums.empty? ? '' : @forums.map(&:id)) ).all
+
+      # Article.where.not(id: ['Rails 3', 'Rails 5'])
+
+      # @discuss_topics = DiscussTopic.joins("LEFT JOIN discuss_categories dc 
+      # ON dc.id = discuss_topics.discuss_category_id").where("dc.bugs = false AND dc.feature_requests = false").order('id desc').select("discuss_topics.*")
+    end
+
     def set_doctor
       @doctor = Doctor.find(params[:id])
     end
