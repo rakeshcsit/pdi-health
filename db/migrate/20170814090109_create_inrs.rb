49f7c9365ccb519f4ad48a1cc4d8c4e4e19a631a
Pavan Katepalli <pavankatepalli@Pavans-MBP-2.home>
2017-08-14 03:08:07 -0700

    fix col type

--- db/migrate/20170814090109_create_inrs.rb
+++ db/migrate/20170814090109_create_inrs.rb
@@ -1,7 +1,7 @@
 class CreateInrs < ActiveRecord::Migration
   def change
     create_table :inrs do |t|
-      t.user :references
+      t.references :user, index: true
       t.decimal :value
       t.text :note
       t.date :wdate
