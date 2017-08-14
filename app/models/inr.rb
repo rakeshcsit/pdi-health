f064b978c96a2a1719df8bda72564193cad9776f
Pavan Katepalli <pavankatepalli@Pavans-MBP-2.home>
2017-08-14 04:51:03 -0700

    modify inr

--- app/models/inr.rb
+++ app/models/inr.rb
@@ -1,3 +1,4 @@
 class Inr < ActiveRecord::Base
-	:belongs_to :user, dependent: :destroy
+	belongs_to :user, dependent: :destroy
+	validates_numericality_of :value, :greater_than_or_equal_to => 0.01, :less_than => 30
 end
