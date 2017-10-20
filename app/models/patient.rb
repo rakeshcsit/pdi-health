ffc32edcb71a4e7343142e8709a7e0a9c89b8775
Pavan Katepalli <pavankatepalli@Pavans-MBP-2.home>
2017-10-20 03:50:27 -0700

    compliance method in works

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -12,6 +12,34 @@ class Patient < ActiveRecord::Base
 
 	validates :first_name, :last_name, :email, :communication_preferred, :phone_number, :address, :state, :zipcode, presence: true
 
+	def compliance_history
+		first_inr = self.user.inrs.first
+
+		if first_inr
+			first_inr_date = first_inr.wdate
+
+			# store
+			next_inr_date = first_inr_date
+			current_date = Date.today
+			period_starts = []
+
+			while next_inr_date <= current_date
+				next_date = next_inr_date + 30
+				next_inr_date = next_date
+				period_starts.push next_date
+			end
+
+			# not completely sure about this
+			if period_starts.last <= current_date
+				period_starts.push period_starts.last + 30
+			end
+
+			return period_starts
+		else
+			return 'Has not reported first INR'
+		end
+	end
+
 	# return number of inrs for this month
 	def inrs_this_month
 		true
